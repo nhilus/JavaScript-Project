9487418a33c18363f34962ab6e8a5f023de166e6
António Barbosa <tobarbosa11@gmail.com>
2022-06-19 22:49:47 +0100

    better css config, corrected item data divs

--- src/components/home-screen.js
+++ src/components/home-screen.js
@@ -21,34 +21,47 @@ const HomeScreen = {
                     (product) =>`
                 <li>
                     <div class="product">
-                    <a href="/#/product/${product.id}">
-                        <img src="${product.image}" alt="${product.title}"/>
-                    </a>
-                    </div>
-                        <div class="product-rating">
-                            ${product.rating}
-                        </div>
+                        <a href="/#/product/${product.id}">
+                            <img src="${product.image}" alt="${product.title}"/>
+                        </a>
                         <div class="product-title">
                             <a href="/#/product/">
                                 ${product.title}
                             </a>
                         </div>
                         <div class="product-category">
-                            <a href="/#/product/">
-                                ${product.category}
-                            </a>
+                            ${product.category}
                         </div>
                         <div class="product-price">
-                            ${product.price}
+                        ${product.price}€
                         </div>
-                        
-                    </div>
                     </div>
                 </li>
             `).join('\n')}
         </ul>
         `
-       
+
+        /*
+        return `
+        <ul class="products">
+            ${products
+                .map(
+                    (product) =>`
+                <div class="row">
+                    <div class="col-4">
+                        <img src="${product.image}">
+                        <h4>${product.name}</h4>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <p>${product.price}</p>
+                    </div>
+            `).join('\n')}
+        </ul>`*/
     }
 }
 
