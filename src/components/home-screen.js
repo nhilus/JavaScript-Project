d4924aea5d7d0d0d45997378ff16ec039fd7fbee
António Barbosa <tobarbosa11@gmail.com>
2022-06-30 04:26:57 +0100

    refactored code, added search functionality, error on toLowerCase() when searching, commented excess code

--- src/components/home-screen.js
+++ src/components/home-screen.js
@@ -1,3 +1,5 @@
+/*
+
 import Rating from "./rating";
 
 const HomeScreen = {
@@ -52,3 +54,56 @@ const HomeScreen = {
 
 export default HomeScreen;
 
+*/
+
+//import Rating from "../components/rating.js";
+/*async function StoreData(){
+    const response = await fetch("https://fakestoreapi.com/products/category/electronics?limit=10")
+    const storeProducts = await response.json();
+    storeProducts.forEach(product => localStorage.setItem('storeProducts', JSON.stringify(product)));
+    // console.log("async await: ", storeProducts);
+};
+
+StoreData();
+
+
+
+console.log("async await: ", storeProducts);
+
+*/
+/*
+const productCardTemplate = document.querySelector("[data-product-tempate")
+const productCardContainer = document.querySelector("[data-products-card-container]");
+searchInput = document.querySelector("[data-search]");
+
+let products = [];
+let rating = Rating;
+
+
+
+searchInput.addEventListener("input", (e)=>{ 
+    const value = e.target.value;
+   
+
+})
+
+fetch("https://fakestoreapi.com/products/category/electronics?limit=10")
+    .then(res =>res.json)
+    .then(data=> {
+        products = data.map(product => {
+            let myImage = new Image(100, 200);
+            myImage.src = product.image;
+            document.body.appendChild(myImage);
+            const card = productCardTemplate.content.cloneNode(true).children[0];
+            const header = card.querySelector("[data-header]");
+            const body = card.querySelector("[data-body]");
+            header = document.appendChild(myImage);
+            body.textContent = product.name, product.price,rating.render();
+            productCardContainer.append(card)
+            return {name: product.name, 
+                    image: product.image, 
+                    price: product.price,
+                    rating: rating.render(), 
+                    element: card}
+        });
+    });*/
--- src/views/store-display.js
+++ src/views/store-display.js
@@ -1,4 +1,4 @@
-import HomeScreen from "../components/home-screen.js";
+/*import HomeScreen from "../components/home-screen.js";
 
 
 const router= async () => {
@@ -8,4 +8,61 @@ const router= async () => {
 };
 
 window.addEventListener("load", router);
+*/
 
+import Rating from "../components/rating";
+
+
+const productCardTemplate = document.querySelector("[data-product-template")
+const productCardContainer = document.querySelector("[data-products-card-container]");
+const searchInput = document.querySelector("[data-search]");
+
+
+
+let products = []
+
+const ratingHtml = Rating.render()
+
+searchInput.addEventListener("input", (e)=>{ 
+    const value = e.target.value.toLowerCase();
+    products.forEach(product => {
+        const isVisible = 
+        product.name.toLowerCase().includes(value) || 
+        product.category.toLowerCase().includes(value);
+        product.element.classList.toggle("hide", !isVisible);
+   })
+
+})
+
+fetch("https://fakestoreapi.com/products/category/electronics?limit=10")
+    .then(res =>res.json())
+    .then(data => {
+        products = data.map(product => {
+
+            const card = productCardTemplate.content.cloneNode(true).children[0];
+            const image = card.querySelector("[data-image]");
+            const body = card.querySelector("[data-body]");
+            const rating = card.querySelector("[data-rating]");
+
+            let productImage = new Image(150, 200);
+            productImage.src = product.image;
+            image.appendChild(productImage);
+
+
+            body.textContent = [product.title,
+                                product.category,
+                                product.price,
+                                ]
+
+            rating.insertAdjacentHTML("beforebegin",
+                ratingHtml);                
+
+            productCardContainer.append(card);
+
+            return {title: product.title,
+                    category: product.category, 
+                    image: product.image, 
+                    price: product.price,
+                    element: card}
+        })
+    })
