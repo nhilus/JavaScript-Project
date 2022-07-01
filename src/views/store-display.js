712a4c79df32fb16f01633a59b534db4886ba442
antonio.barbosa <tobarbosa11@gmail.com>
2022-07-01 20:45:32 +0100

    fetch corrected and separated from render, array stored into local storage, card rendered through local storage,

--- src/views/store-display.js
+++ src/views/store-display.js
@@ -1,43 +1,45 @@
-/*import HomeScreen from "../components/home-screen.js";
-
-
-const router= async () => {
-    const main = document.getElementById("main-container");
-    main.innerHTML = await HomeScreen.render();
-
-};
-
-window.addEventListener("load", router);
-*/
-
 import Rating from "../components/rating";
 
-
-const productCardTemplate = document.querySelector("[data-product-template")
+const productCardTemplate = document.querySelector("[data-product-template");
 const productCardContainer = document.querySelector("[data-products-card-container]");
 const searchInput = document.querySelector("[data-search]");
+const url = ('https://fakestoreapi.com/products/category/electronics?limit=10');
+let products = [];
+const ratingHtml = Rating.render();
 
 
-
-let products = []
-
-const ratingHtml = Rating.render()
+//search bar filter function
 
 searchInput.addEventListener("input", (e)=>{ 
     const value = e.target.value.toLowerCase();
     products.forEach(product => {
-        const isVisible = 
-        product.name.toLowerCase().includes(value) || 
-        product.category.toLowerCase().includes(value);
+        const isVisible = product.name?.toLowerCase().includes(value) || product.category?.toLowerCase().includes(value);
         product.element.classList.toggle("hide", !isVisible);
-   })
+   });
 
-})
+});
 
-fetch("https://fakestoreapi.com/products/category/electronics?limit=10")
-    .then(res =>res.json())
-    .then(data => {
-        products = data.map(product => {
+// turn into api component
+
+async function getData(){
+    const response = await fetch(url);
+    const data =  await response.json();
+    data.forEach(product => {
+        products.push(product);
+    })
+        localStorage.setItem("productDetails", JSON.stringify(products));
+    };
+
+    
+
+    console.log(products);
+
+// turn into card compopnent
+
+        function renderProducts() {
+            const productcards = JSON.parse(localStorage.getItem("productDetails"));
+        
+            productcards.forEach(product => {
 
             const card = productCardTemplate.content.cloneNode(true).children[0];
             const image = card.querySelector("[data-image]");
@@ -48,21 +50,21 @@ fetch("https://fakestoreapi.com/products/category/electronics?limit=10")
             productImage.src = product.image;
             image.appendChild(productImage);
 
-
             body.textContent = [product.title,
-                                product.category,
-                                product.price,
+                                product.price
                                 ]
-
-            rating.insertAdjacentHTML("beforebegin",
-                ratingHtml);                
-
+            rating.insertAdjacentHTML("beforebegin", ratingHtml);                
             productCardContainer.append(card);
 
-            return {title: product.title,
-                    category: product.category, 
+            return {title: product.title, 
                     image: product.image, 
                     price: product.price,
                     element: card}
-        })
-    })
+        });
+    };
+
+//anonymous function immediately-invoked
+    (async () =>{
+        await getData();
+        renderProducts();
+    })()
